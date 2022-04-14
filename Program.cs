e423e26a1e966135d03b75fdf3726bc9b9950738
Kuba Kusiak <thevipgamerpro@gmail.com>
2022-04-14 09:16:34 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnimalNoises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalNoise[] animals= new AnimalNoise[2];
+            animals[0] = new Dog();
+            animals[1] = new Cow();
+
+            for(int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine(animals[i].getNoise());
+            }
+        }
+       
+    }
+}
